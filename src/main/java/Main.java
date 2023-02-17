43c2aad00bf3a1c891073e86ab9f0a56c5d81631
“Koluka” <koluka78@mail.ru>
2023-02-17 17:09:03 +0300

    AddTest2

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,3 @@
-public class Main {
-    public static void main(String[] args) {
-        StatisticsService service = new StatisticsService();
-        long[] incomes = {4, 5, 1, 6, 4};
-        int rezult = (int) service.findMax(incomes);
-        System.out.print(rezult);
-    }
-    }
+
 
 
--- src/test/java/StatisticsServiceTest.java
+++ src/test/java/StatisticsServiceTest.java
@@ -16,6 +16,19 @@ public class StatisticsServiceTest {
 
     }
 
+    @Test
+    void findMaxInMiddle() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 60, 8, 6, 11, 11, 12};
+        long expected = 60;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+
+    }
+
 }
 
 
